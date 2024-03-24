7107457238c7fcbed96e705b1c5d8bd34ae30339
Chandler Reynolds <arglebargle75@gmail.com>
2024-03-24 14:14:27 -0400

    Added Average and Positive percentage per exercise 1.7

--- unicafe/src/App.jsx
+++ unicafe/src/App.jsx
@@ -12,19 +12,31 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [all, setAll] = useState(0)
+
+  const incrementValue = (setFunction, stateValue) => {   //Increment the passed state hook by 1, and increment the all count by 1
+    setFunction(stateValue + 1)
+    setAll(all + 1)
+
+  }
 
   return (
     <div>
       <h1>Give feedback</h1>
-      <Button handleClick={() => setGood(good + 1)} text = "good" />
-      <Button handleClick={() => setNeutral(neutral + 1)} text = "neutral"/>
-      <Button handleClick={() => setBad(bad + 1)} text = "bad"/>
+
+      <Button handleClick={() => incrementValue(setGood, good)} text = "good" />
+      <Button handleClick={() => incrementValue(setNeutral, neutral)} text = "neutral"/>
+      <Button handleClick={() => incrementValue(setBad, bad)} text = "bad"/>
 
       <h1>Statistics</h1>
 
       <Display text={"good: " + good}/>
       <Display text={"neutral: " + neutral}/>
       <Display text={"bad: " + bad}/>
+      <Display text={"all: " + all}/>
+
+      <Display text={"average: " + (good-bad)/all}/>
+      <Display text={"positive: " + good / all * 100 + "%"}/>
 
     </div>
   )
